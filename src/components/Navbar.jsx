25bd3d9ea0e047b0a09d7f3be6dd63a2c86e2418
Ananthbarath <ananthbarath2001@gmail.com>
2025-01-26 14:23:32 +0530

    final

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,42 @@
-import "../styles/Navbar.scss"
-const Navbar = () => {
+import { useEffect, useState } from "react";
+import "../styles/Navbar.scss";
 
-  
+const Navbar = () => {
+  const [isSideMenuOpen, setSideMenuOpen] = useState(false);
+  const [scrolled, setScrolled] = useState(false);
+
+  const handleScroll = () => {
+    const offset = window.scrollY;
+    if(offset > 60) setScrolled(true);
+    else setScrolled(false);
+  }
+
+  const showSideMenu = () => setSideMenuOpen(true);
+  const hideSideMenu = () => setSideMenuOpen(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <nav className={`navbar flex items-center`}>
+    <nav className={`navbar flex items-center ${scrolled ? "scrolled" : ""}`}>
       <div className="container navbar-container w-full flex justify-between">
-        <div className="brand-and-together flex items-center jusitfy-between">
-          <a href="#" className="nav-brand text-white">Bloog <span className="nav-brand-dot bg-white"></span>
+        <div className="brand-and-toggler flex items-center justify-between">
+          <a href="#" className="nav-brand text-white">
+            Bloog <span className="nav-brand-dot bg-white"></span>
           </a>
-          <button type="button" className="nav-show-btn text-white">
+          <button type="button" className="nav-show-btn text-white" onClick={showSideMenu}>
             <i className="bi bi-list"></i>
           </button>
-
         </div>
-        <div className={`nb-menu-wrapper flex items-center`}>
-          <button type="button" className="nav-hide-btn">
+
+        <div className={`nb-menu-wrapper flex items-center ${isSideMenuOpen ? "show" : ""}`}>
+          <button type="button" className="nav-hide-btn" onClick={hideSideMenu}>
             <i className="bi bi-x-square"></i>
           </button>
           <ul className="nav-menu flex items-center">
             <li className="nav-item">
-              <a href="#" className="nav-link">
-                Home
-              </a>
+              <a href="#" className="nav-link">Home</a>
             </li>
             <li className="nav-item">
               <a href="#" className="nav-link">
@@ -32,29 +47,20 @@ const Navbar = () => {
               </a>
             </li>
             <li className="nav-item">
-              <a href="#" className="nav-link">
-                Archives
-              </a>
+              <a href="#" className="nav-link">Archives</a>
             </li>
             <li className="nav-item">
-              <a href="#" className="nav-link">
-                About Us
-              </a>
+              <a href="#" className="nav-link">About Us</a>
             </li>
             <li className="nav-item">
-              <a href="#" className="nav-link">
-                Contact
-              </a>
+              <a href="#" className="nav-link">Contact</a>
             </li>
-            
           </ul>
           <div className="nav-btns">
-            <a href="#" className="nav-btn btn">Get it now</a>
+              <a href="#" className="nav-btn btn">Get it now</a>
           </div>
-
         </div>
       </div>
-
     </nav>
   )
 }
